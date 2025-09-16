d6f3d4a1be1e52f476d1e7c5aeb8fc56e28a8705
mkorwel <matt.korwel@gmail.com>
2025-09-16 14:02:53 -0700

    feat(ci): add explicit vitest config for test-utils

---
+++ packages/test-utils/vitest.config.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    reporters: ['default', 'junit'],
+    silent: true,
+    outputFile: {
+      junit: 'junit.xml',
+    },
+    minThreads: 8,
+    maxThreads: 16,
+  },
+});
